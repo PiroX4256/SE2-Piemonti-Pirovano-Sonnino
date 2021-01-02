3657da9d44e661fb41bf02b6161c2ab737be8135
Luca Pirovano <luca.pirovano.98@gmail.com>
2021-01-02 23:18:21 +0100

    feat: IT project

---
+++ IT/Implementation/src/test/java/it/polimi/se2/clupapplication/ClupapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polimi.se2.clupapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClupapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
