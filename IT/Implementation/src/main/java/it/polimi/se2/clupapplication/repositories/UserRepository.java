7b136e26ee6e8b138d9104f3c2ef1bf8d7ead5b6
Luca Pirovano <luca.pirovano.98@gmail.com>
2021-01-03 23:31:42 +0100

    feat: json web token authentication

--- IT/Implementation/src/main/java/it/polimi/se2/clupapplication/repositories/UserRepository.java
+++ IT/Implementation/src/main/java/it/polimi/se2/clupapplication/repositories/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    User findByToken(String token);
 }
---
+++ IT/Implementation/src/main/java/it/polimi/se2/clupapplication/utils/JwtResponse.java
@@ -0,0 +1,16 @@
+package it.polimi.se2.clupapplication.utils;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+
+public class JwtResponse implements Serializable {
+    private static final long serialVersionUID = -8091879091924046844L;
+    private final String jwttoken;
+    public JwtResponse(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+    public String getToken() {
+        return this.jwttoken;
+    }
+}
