dfbfca3be695e8f240183fe31d8e17a6d0eab0e8
Luca Pirovano <luca.pirovano.98@gmail.com>
2021-01-22 21:43:49 +0100

    te lo pusho

--- IT/Implementation/Backend/src/main/resources/public/static/js/login/signup.js
+++ IT/Implementation/Backend/src/main/resources/public/static/js/login/signup.js
@@ -19,37 +19,52 @@ var signupContainer = new Vue({
 
 function submitForm() {
     var form = document.signUpForm;
-    let role, jsonReq;
-    if(signupContainer.manager) {
-        role = "MANAGER";
-        jsonReq = {username: form.username.value, password: form.password.value, name: form.name.value, surname: form.surname.value, role: role}
-    } else if(signupContainer.attendant) {
-        role = "ATTENDANT";
-        jsonReq = {username: form.username.value, password: form.password.value, name: form.name.value, surname: form.surname.value, role: role, storeId: form.storeId.value}
-    } else {
-        role = "USER";
-        jsonReq = {username: form.username.value, password: form.password.value, role: role}
-    }
-    if(form.password.value != form.confirmPassword.value) {
-        alert("Passwords do not match!");
-        return;
-    }
-    $.ajax({
-        url: '/api/auth/signup',
-        method: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(jsonReq),
-        success: function (data) {
-            alert("Registration successful! You will be now redirected to login page.");
-            window.location.href = '/login';
-        },
-        error: function (err) {
-            if(err.status == 400) {
-                alert("Error during the sign up process. The profile with the provided username already exists in our systems.");
-            } else {
-                alert("Error during the sign up process. Please check all the fields and try again. Error: ");
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(form.username.value)) {
+        let role, jsonReq;
+        if (signupContainer.manager) {
+            role = "MANAGER";
+            jsonReq = {
+                username: form.username.value,
+                password: form.password.value,
+                name: form.name.value,
+                surname: form.surname.value,
+                role: role
             }
-            console.log(err);
+        } else if (signupContainer.attendant) {
+            role = "ATTENDANT";
+            jsonReq = {
+                username: form.username.value,
+                password: form.password.value,
+                name: form.name.value,
+                surname: form.surname.value,
+                role: role,
+                storeId: form.storeId.value
+            }
+        } else {
+            role = "USER";
+            jsonReq = {username: form.username.value, password: form.password.value, role: role}
         }
-    });
+        if (form.password.value != form.confirmPassword.value) {
+            alert("Passwords do not match!");
+            return;
+        }
+        $.ajax({
+            url: '/api/auth/signup',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(jsonReq),
+            success: function (data) {
+                alert("Registration successful! You will be now redirected to login page.");
+                window.location.href = '/login';
+            },
+            error: function (err) {
+                if (err.status == 400) {
+                    alert("Error during the sign up process. The profile with the provided username already exists in our systems.");
+                } else {
+                    alert("Error during the sign up process. Please check all the fields and try again. Error: ");
+                }
+                console.log(err);
+            }
+        });
+    }
 }
