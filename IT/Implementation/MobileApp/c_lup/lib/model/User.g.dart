6a390e49658ced26b01213501d0e74f003cd57cf
Sonny <swagpino@gmail.com>
2021-01-29 21:51:52 +0100

    (ノಠ益ಠ)ノ彡┻━┻

--- IT/Implementation/MobileApp/c_lup/lib/model/User.g.dart
+++ IT/Implementation/MobileApp/c_lup/lib/model/User.g.dart
@@ -19,19 +19,26 @@ class UserAdapter extends TypeAdapter<User> {
     return User(
       token: fields[0] as String,
       role: fields[2] as String,
-    )..reservations = (fields[1] as List)?.cast<Reservation>();
+      reservations: (fields[1] as List)?.cast<Reservation>(),
+      stores: (fields[3] as List)?.cast<Store>(),
+      storeId: fields[4] as String,
+    );
   }
 
   @override
   void write(BinaryWriter writer, User obj) {
     writer
-      ..writeByte(3)
+      ..writeByte(5)
       ..writeByte(0)
       ..write(obj.token)
       ..writeByte(1)
       ..write(obj.reservations)
       ..writeByte(2)
-      ..write(obj.role);
+      ..write(obj.role)
+      ..writeByte(3)
+      ..write(obj.stores)
+      ..writeByte(4)
+      ..write(obj.storeId);
   }
 
   @override
